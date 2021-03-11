b63198ea21a66a52c453ef4b322f8277a6db1581
vanlintbram <vanlintbram@hotmail.com>
2021-03-11 18:36:09 +0000

    LABO 6.2.7

--- mapquest/mapquest_parse-json_2.py
+++ mapquest/mapquest_parse-json_2.py
@@ -7,19 +7,13 @@ dest = ""
 key = "m1EAJciU24VbAnfGJ0PdRiAABbPTGyJ2"
 
 
-while orig == "" and dest == "":
-    orig = input("Give your starting point as eg 'Grimbergen, Belgium'")
-    dest = input("Give your destination point as eg 'Grimbergen, Belgium'")
-
-
-url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest}) 
-
-print("URL: " + (url))
-
-json_data = requests.get(url).json()
-json_status = json_data["info"]["statuscode"]
-
-if json_status == 0:
-    print("API Status: " + str(json_status) + " = A successful route call.\n")
-
+while True:
+   orig = input("Starting Location: ")
+   dest = input("Destination: ")
+   url = main_api + urllib.parse.urlencode({"key": key, "from":orig, "to":dest})
+   print("URL: " + (url))
+   json_data = requests.get(url).json()
+   json_status = json_data["info"]["statuscode"]
+   if json_status == 0:
+       print("API Status: " + str(json_status) + " = A successful route call.\n")
 
